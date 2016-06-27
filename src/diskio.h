393bfb59b2a10275a34eea1627d28111508a97a0
WMXZ-EU <walter@wmxz.eu>
2016-06-27 16:33:57 +0200

    mods
    mods for updating github

--- src/diskio.h
+++ src/diskio.h
@@ -37,7 +37,6 @@ DRESULT disk_read (BYTE pdrv, BYTE* buff, DWORD sector, UINT count);
 DRESULT disk_write (BYTE pdrv, const BYTE* buff, DWORD sector, UINT count);
 DRESULT disk_ioctl (BYTE pdrv, BYTE cmd, void* buff);
 
-
 /* Disk Status Bits (DSTATUS) */
 
 #define STA_NOINIT		0x01	/* Drive not initialized */
