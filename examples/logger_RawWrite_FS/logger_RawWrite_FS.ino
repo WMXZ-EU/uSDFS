7bec6c67f0d0dff750e17d1b1700aa24c808e565
WMXZ-EU <walter@wmxz.eu>
2019-05-15 14:11:07 +0200

    Create logger_RawWrite_FS.ino

---
+++ examples/logger_RawWrite_FS/logger_RawWrite_FS.ino
@@ -0,0 +1,452 @@
+//Copyright 2019 by Walter Zimmer
+// Version 15-may-19
+//
+/*
+ * Logger test program
+ * to test Stock SD library, Bill Greimans SdFS, CHaN SDFAT (WMXZ: uSDFS) 
+ * to test SPI, SDIO, USBHost
+ * to test FAT32, and exFAT
+ * AS of 15-may-2019 following implementations are known
+ * Stock SD library:     Fat32;         SPI(T3,T4), SDIO(T3,T4)
+ * Bill Greimans SdFS:   Fat32, exFAT;  SPI(T3,T4), SDIO(T3)
+ * WMXZ uSDFS:           FAT32, exFAT;  SPI(T3,T4), SDIO(I3,T4), USBHost(T3,T4)
+ * Caveat: functionality only partial verified
+ */
+#define TEST_DRV 0 // 0: SPI; 1: SDIO; 2: USBHost
+
+//
+#define MXFN 1 // maximal number of files //was 100
+#define MXRC 2 // number of records in file // was 1000
+char *fnamePrefix = "A";
+
+#define SDo 1   // Stock SD library
+#define SdFS 2  // Greimans SD library
+#define uSDFS 3 // CHaN's SD library (WMXZ's uSDFS)
+
+#define USE_FS SdFS
+
+#if TEST_DRV == 0
+  #define SD_CS 10
+  #define SD_SCK 14
+  #define SD_MOSI 7
+  #define SD_MISO 12
+#elif TEST_DRV == 1
+  #define SD_CS BUILTIN_SDCARD
+#endif
+
+/* MSF API 
+ *  init(void);
+ *  void exit(void);
+ *  void open(char * filename);
+ *  void close(void);
+ *  uint32_t write(uint8_t *buffer, uint32_t nbuf);
+ *  uint32_t read(uint8_t *buffer, uint32_t nbuf);
+ */
+
+//--------------------- For File Time settings ------------------
+#if USE_FS != uSDFS
+  #include <time.h>
+  #define EPOCH_YEAR 1970 //T3 RTC
+  #define LEAP_YEAR(Y) (((EPOCH_YEAR+Y)>0) && !((EPOCH_YEAR+Y)%4) && ( ((EPOCH_YEAR+Y)%100) || !((EPOCH_YEAR+Y)%400) ) )
+  static  const uint8_t monthDays[]={31,28,31,30,31,30,31,31,30,31,30,31}; 
+  
+  /*  int  tm_sec;
+    int tm_min;
+    int tm_hour;
+    int tm_mday;
+    int tm_mon;
+    int tm_year;
+    int tm_wday;
+    int tm_yday;
+    int tm_isdst;
+  */
+  
+  struct tm seconds2tm(uint32_t tt)
+  { struct tm tx;
+    tx.tm_sec   = tt % 60;    tt /= 60; // now it is minutes
+    tx.tm_min   = tt % 60;    tt /= 60; // now it is hours
+    tx.tm_hour  = tt % 24;    tt /= 24; // now it is days
+    tx.tm_wday  = ((tt + 4) % 7) + 1;   // Sunday is day 1 (tbv)
+  
+    // tt is now days since EPOCH_Year (1970)
+    uint32_t year = 0;  
+    uint32_t days = 0;
+    while((unsigned)(days += (LEAP_YEAR(year) ? 366 : 365)) <= tt) year++;
+  
+    tx.tm_year = 1970+year; // year is offset from 1970 
+  
+    // correct for last (actual) year
+    days -= (LEAP_YEAR(year) ? 366 : 365);
+    tt  -= days; // now tt is days in this year, starting at 0
+    
+    uint32_t month=0;
+    uint32_t monthLength=0;
+    for (month=0; month<12; month++) 
+    { monthLength = monthDays[month];
+      if ((month==1) & LEAP_YEAR(year)) monthLength++; 
+      if (tt<monthLength) break;
+      tt -= monthLength;
+    }
+    tx.tm_mon = month + 1;   // jan is month 1  
+    tx.tm_mday = tt + 1;     // day of month
+    return tx;
+  }
+  
+  uint32_t tm2seconds (struct tm *tx) 
+  {
+    uint32_t tt;
+    tt=tx->tm_sec+tx->tm_min*60+tx->tm_hour*3600;  
+  
+    // count days size epoch until previous midnight
+    uint32_t days=tx->tm_mday;
+  
+    uint32_t mm=0;
+    uint32_t monthLength=0;
+    for (mm=0; mm<(tx->tm_mon-1); mm++) days+=monthDays[mm]; 
+    if(tx->tm_mon>2 && LEAP_YEAR(tx->tm_year-1970)) days++;
+  
+    uint32_t years=0;
+    while(years++ < (tx->tm_year-1970)) days += (LEAP_YEAR(years) ? 366 : 365);
+    //  
+    tt+=(days*24*3600);
+    return tt;
+  }
+#endif
+
+#if  USE_FS == SDo
+  #include "SPI.h"
+  #include "SD.h"
+  
+  class mFS_class
+  {
+    private:
+    File file;
+  
+    void die(char *txt) {Serial.println(txt); while(1) asm("wfi");}
+    
+    public:
+      void init(void)
+      { 
+        Serial.println("Using SDo");
+        #if TEST_DRV==0
+          // Initialize the SD card
+          SPI.setMISO(SD_MISO);
+          SPI.setMOSI(SD_MOSI);
+          SPI.setSCK(SD_SCK);
+        #endif
+        int rc;
+        if (!(rc=SD.begin(SD_CS))) {Serial.println(rc); die("error SD.begin");}
+      }
+  
+      void mkDir(char * dirname)  {  SD.mkdir(dirname);  }
+        
+      void exit(void) { }
+      
+      void open(char * filename) {  file = SD.open(filename, FILE_WRITE);  }
+  
+      void close(void)  {  file.close(); }
+  
+      uint32_t write(uint8_t *buffer, uint32_t nbuf)
+      {
+        file.write(buffer, nbuf);
+        return nbuf;
+      }
+  
+      uint32_t read(uint8_t *buffer, uint32_t nbuf)
+      {      
+        if ((int)nbuf != file.read(buffer, nbuf)) die("error file read");
+        return nbuf;
+      }
+  };
+
+#elif USE_FS == SdFS
+
+  #include "SdFs.h"
+
+  #if defined(__IMXRT1062__)
+    #error "SDFS not ported yet to IMXRT1062"
+  #endif
+  //
+  #if TEST_DRV == 0
+    #define SD_CS 10
+    #define SD_CONFIG SdSpiConfig(SD_CS, DEDICATED_SPI, SPI_FULL_SPEED)
+  #elif TEST_DRV ==1
+    #define SD_CONFIG SdioConfig(FIFO_SDIO)
+    //#define SD_CONFIG SdioConfig(DMA_SDIO)
+  #elif TEST_DRV == 2
+    #error "USB_HOST not implemented"
+  #endif 
+
+  // Call back for file timestamps.  Only called for file create and sync().
+  void dateTime(uint16_t* date, uint16_t* time) 
+  {
+    struct tm tx=seconds2tm(RTC_TSR);
+      
+    // Return date using FS_DATE macro to format fields.
+    *date = FS_DATE(tx.tm_year, tx.tm_mon, tx.tm_mday);
+  
+    // Return time using FS_TIME macro to format fields.
+    *time = FS_TIME(tx.tm_hour, tx.tm_min, tx.tm_sec);
+  }
+
+  class mFS_class
+  {
+    private:
+    SdFs sd;
+    FsFile file;
+    
+    public:
+      void init(void)
+      { Serial.println("Using SdFS");
+        #if TEST_DRV==0
+          // Initialize the SD card
+          SPI.setMISO(SD_MISO);
+          SPI.setMOSI(SD_MOSI);
+          SPI.setSCK(SD_SCK);
+        #endif
+        if (!sd.begin(SD_CONFIG)) sd.errorHalt("sd.begin failed");
+      
+        // Set Time callback
+        FsDateTime::callback = dateTime;
+      }
+  
+      void mkDir(char * dirname)  { if(!sd.exists(dirname)) sd.mkdir(dirname);  }
+      
+      void chDir(char * dirname)  { sd.chdir(dirname);  }
+      
+      void exit(void) { sd.end(); }
+      
+      void open(char * filename)
+      {
+        if (!file.open(filename, O_CREAT | O_TRUNC |O_RDWR)) {
+          sd.errorHalt("file.open failed");
+        }
+        if (!file.preAllocate(PRE_ALLOCATE_SIZE)) {
+          sd.errorHalt("file.preAllocate failed");    
+        }
+      }
+  
+      void close(void)
+      {
+        file.truncate();
+        file.close();
+      }
+  
+      uint32_t write(uint8_t *buffer, uint32_t nbuf)
+      {
+        if (nbuf != file.write(buffer, nbuf)) sd.errorHalt("write failed");
+        return nbuf;
+      }
+  
+      uint32_t read(uint8_t *buffer, uint32_t nbuf)
+      {      
+        if ((int)nbuf != file.read(buffer, nbuf)) sd.errorHalt("read failed");
+        return nbuf;
+      }
+  };
+
+#elif  USE_FS == uSDFS
+  #include "uSDFS.h"
+
+  #if TEST_DRV == 0
+    const char *Dev = "0:/";  // SPI
+  #elif TEST_DRV == 1
+    const char *Dev = "1:/";  // SDHC
+  #elif TEST_DRV == 2
+    const char *Dev = "2:/";  // USB
+  #endif
+  
+  class mFS_class
+  {
+    private:
+      FRESULT rc;     /* Result code */
+      FATFS fatfs;    /* File system object */
+      FIL fil;        /* File object */
+  
+      UINT wr;
+      
+      /* Stop with error message */
+      void die(const char *text, FRESULT rc)
+      { Serial.printf("%s: Failed with rc=%s.\r\n", text,FR_ERROR_STRING[rc]);  while(1) asm("wfi"); }
+      
+    public:
+      void init(void)
+      {
+        Serial.println("Using uSDFS");
+        Serial.print("uSDFS_VER:"); Serial.println(uSDFS_VER);
+        if((rc = f_mount (&fatfs, Dev, 1))) die("Mount",rc);      /* Mount/Unmount a logical drive */
+      
+        Serial.printf("File System %s\n", fileSystem[fatfs.fs_type]);
+        Serial.printf("Free Disk Size %d clusters\n",fatfs.free_clst);
+        Serial.printf("Cluster Size %d sectors\n",fatfs.csize);
+        #if FF_MAX_SS != FF_MIN_SS
+          Serial.printf("Sector Size %d bytes\n",fatfs.ssize);
+        #else
+          Serial.printf("Sector Size %d bytes\n",FF_MIN_SS);
+        #endif
+        
+        Serial.printf("\nChange drive\n");
+        if((rc = f_chdrive(Dev))) die("chdrive",rc);
+      }
+  
+      void mkDir(char * dirname)
+      {
+        
+      }
+  
+      void exit(void)
+      {
+        rc = f_unmount(Dev);
+        Serial.print("unmount "); Serial.println(FR_ERROR_STRING[rc]);        
+      }
+      
+      void open(char * filename)
+      {
+        // check status of file
+        rc = f_stat(filename,0);
+        Serial.printf("stat %s %x\n",FR_ERROR_STRING[rc],fil.obj.sclust);
+    
+        rc = f_open(&fil, filename, FA_WRITE | FA_CREATE_ALWAYS);
+        Serial.printf(" opened %s %x\n\r",FR_ERROR_STRING[rc],fil.obj.sclust);
+        // check if file is Good
+        if(rc == FR_INT_ERR)
+        { // only option is to close file
+            rc = f_close(&fil);
+            if(rc == FR_INVALID_OBJECT)
+            { Serial.println("unlinking file");
+              rc = f_unlink(filename);
+              if (rc) die("unlink", rc);
+            }
+            else
+              die("close", rc);
+          // retry open file
+          if(rc = f_open(&fil, filename, FA_WRITE | FA_CREATE_ALWAYS)) die("open", rc);
+        }
+      }
+      
+      void close(void)
+      {
+        //close file
+        if (rc = f_close(&fil)) die("close", rc);
+      }
+      
+      uint32_t write( uint8_t *buffer, uint32_t nbuf)
+      {
+         rc = f_write(&fil, buffer, nbuf, &wr);
+         uint32_t tb=micros();
+         if (rc == FR_DISK_ERR) // IO error
+         {  // only option is to close file
+            // force closing file
+            return 0;
+         }
+         return nbuf;
+      }
+      
+      uint32_t read(uint8_t *buffer, uint32_t nbuf)
+      { return 0;
+      }
+  };
+#endif
+
+class mFS_class mfs;
+
+#if defined(__MK20DX256__)
+  #define BUFFSIZE (2*1024) // size of buffer to be written
+#elif defined(__MK66FX1M0__)
+  #define BUFFSIZE (8*1024) // size of buffer to be written
+#elif defined(__IMXRT1062__)
+  #define BUFFSIZE (8*1024) // size of buffer to be written
+#endif
+
+uint32_t buffer[BUFFSIZE];
+//=========================================================================
+void blink(uint16_t msec) { digitalWriteFast(13,!digitalReadFast(13)); delay(msec); }
+
+void setup()
+{
+  // wait for serial line to come up
+  pinMode(13,OUTPUT);
+  pinMode(13,HIGH);
+
+  while(!Serial);
+  Serial.println("Test logger_RawWrite");
+  Serial.print("BUFFSIZE :");  Serial.println(BUFFSIZE);
+  Serial.print("Dev Type :");  Serial.println(TEST_DRV);
+  mfs.init();
+}
+
+void loop()
+{
+  static uint32_t count=0;
+  static uint32_t ifn=0;
+  static uint32_t isFileOpen=0;
+  static char filename[80];
+  static uint32_t t0=0;
+  static uint32_t t1=0;
+  static uint32_t dtwmin=1<<31, dtwmax=0;
+  static uint32_t dto=1<<31, dtc=0;
+
+  if(ifn>MXFN) { blink(500); return; }
+  
+  if(!count)
+  {
+    // close file
+    if(isFileOpen)
+    { dtc = micros();
+      //close file
+      mfs.close();
+      //
+      isFileOpen=0;
+      t1=micros();
+      dtc = t1-dtc;
+      float MBs = (MXRC*BUFFSIZE*4.0f)/(1.0f*(t1-t0));
+      Serial.printf(" (%d - %f MB/s)\n (open: %d us; close: %d us; write: min,max: %d %d us)\n\r",
+                        t1-t0,MBs, dto, dtc, dtwmin,dtwmax);
+      dtwmin=1<<31; dtwmax=0;
+    }
+  }
+    
+  //
+  if(!isFileOpen)
+  {
+    // open new file
+    ifn++;
+    if(ifn>MXFN) 
+    { mfs.exit();
+      pinMode(13,OUTPUT); return; 
+    } // at end of test: prepare for blinking
+
+    dto=micros();
+    sprintf(filename,"%s_%05d.dat",fnamePrefix,ifn);
+    Serial.println(filename);
+    //
+    mfs.open(filename);
+    //
+    isFileOpen=1;
+    t0=micros();
+    dto=t0-dto;
+  }
+  
+  if(isFileOpen)
+  {
+     // fill buffer
+     for(int ii=0;ii<BUFFSIZE;ii++) buffer[ii]='0'+(count%10);
+     count++;
+     //write data to file 
+     if(!(count%10))Serial.printf(".");
+     if(!(count%640)) Serial.println(); Serial.flush();
+     //
+     uint32_t ta=micros();
+     if(!mfs.write((uint8_t *)buffer,BUFFSIZE*4))
+     {  Serial.printf(" write error at count # %d\n",count);
+        count=MXRC;
+     }
+     uint32_t tb=micros();
+     //    
+     uint32_t dt=tb-ta;
+     if(dt<dtwmin) dtwmin=dt;
+     if(dt>dtwmax) dtwmax=dt;
+     //
+     count %= MXRC;
+  }    
+}
