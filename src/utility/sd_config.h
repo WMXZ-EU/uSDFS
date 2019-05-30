1c21931018c57ba3973770faf6367edcd6421e22
WMXZ-EU <walter@wmxz.eu>
2019-05-30 14:56:54 +0200

    Update sd_config.h
    Added "PARTITION VolToPart[]"

--- src/utility/sd_config.h
+++ src/utility/sd_config.h
@@ -1,4 +1,28 @@
-
+/*
+ * WMXZ Teensy uSDFS library
+ * Copyright (c) 2016, 2019 Walter Zimmer.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining
+ * a copy of this software and associated documentation files (the
+ * "Software"), to deal in the Software without restriction, including
+ * without limitation the rights to use, copy, modify, merge, publish,
+ * distribute, sublicense, and/or sell copies of the Software, and to
+ * permit persons to whom the Software is furnished to do so, subject to
+ * the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS
+ * BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN
+ * ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+ * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+ 
 #ifndef _SD_CONIG_H
 #define _SD_CONIG_H
 
@@ -6,10 +30,21 @@
 extern "C" {
 #endif
 
+#include "ff.h"
+
 /* Definitions of physical drive number for each drive */
 #define DEV_SPI   0 /* Example: Map SPI card to physical drive 0 */
 #define DEV_SDHC  1 /* Example: Map SDHC card to physical drive 1 */
 #define DEV_MSC   2 /* Example: Map MSC card (USB disk) to physical drive 2 */
+
+#if FF_MULTI_PARTITION		/* Multiple partition configuration */ 
+	PARTITION VolToPart[] = {{DEV_SPI, 0}, //{ physical drive number, Partition: 0:Auto detect, 1-4:Forced partition)} 
+	                         {DEV_SDHC,0}, 
+							 {DEV_USB, 0}, 
+							 {DEV_USB, 1}, 
+							 {DEV_USB, 2}
+							 }; /* Volume - Partition resolution table */
+#endif
 
 // SPI 
 #define CS_PIN 10
