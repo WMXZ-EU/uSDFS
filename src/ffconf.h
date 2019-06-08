6bb324c327d9d6ea28e9e4fea396a6164e0fec2b
WMXZ-EU <walter@wmxz.eu>
2019-06-08 16:09:42 +0200

    Update ffconf.h

--- src/ffconf.h
+++ src/ffconf.h
@@ -163,7 +163,7 @@
 / Drive/Volume Configurations
 /---------------------------------------------------------------------------*/
 
-#define FF_VOLUMES		4
+#define FF_VOLUMES		10
 /* Number of volumes (logical drives) to be used. (1-10) */
 
 
